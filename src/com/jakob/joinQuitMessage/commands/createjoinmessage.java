8db8bd25ed9d749f3b12043cd49d99d04224e45e
Unknown <JFriesacher@gmx.at>
2018-08-06 15:15:35 +0200

    version 1.0

--- src/com/jakob/joinQuitMessage/commands/createjoinmessage.java
+++ src/com/jakob/joinQuitMessage/commands/createjoinmessage.java
@@ -30,6 +30,11 @@ public class createjoinmessage implements CommandExecutor {
                     File file = new File(directory.getPath() + "\\join.txt");
                     try {
                         String out = "";
+                        if(!args[args.length-1].contains("&"))
+                        {
+                            p.sendMessage("Es muss 'text &colorcode' als text genommen werden! (@p ist der Username)");
+                            return false;
+                        }
                         String color = args[args.length - 1].replace("&", "").trim();
                         char colorChar = color.charAt(0);
                         if(!((colorChar >= '0' && colorChar <= '9') || (colorChar >= 'a' && colorChar <= 'f')))
--- src/com/jakob/joinQuitMessage/commands/createquitmessage.java
+++ src/com/jakob/joinQuitMessage/commands/createquitmessage.java
@@ -29,6 +29,11 @@ public class createquitmessage implements CommandExecutor {
                     File file = new File(directory.getPath() + "\\quit.txt");
                     try {
                         String out = "";
+                        if(!args[args.length-1].contains("&"))
+                        {
+                            p.sendMessage("Es muss 'text &colorcode' als text genommen werden! (@p ist der Username)");
+                            return false;
+                        }
                         String color = args[args.length - 1].replace("&", "").trim();
                         char colorChar = color.charAt(0);
                         if(!((colorChar >= '0' && colorChar <= '9') || (colorChar >= 'a' && colorChar <= 'f')))
